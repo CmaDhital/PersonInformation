d7d687e5a497b9f35041070bd05ca9df0c76440c
Seema Dhital <cmadhital@outlook.com>
2016-09-27 16:01:12 -0500

    Working on validaton

--- js/controllers/formCtrl.js
+++ js/controllers/formCtrl.js
@@ -1,4 +1,9 @@
 angular.module('app').controller('formCtrl', ['$scope', function($scope){
     
-    
+  
+    $scope.first="Seema";
+        $scope.last="Dhital";       
+        $scope.address="Irving,Texas";
+         $scope.phone="3344929634";
+
 }]);
